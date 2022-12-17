0f89c5649e4847ef0f5fec19ff92a00095d85854
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-17 18:29:23 -0300

    added first draft of pint

---
+++ pint.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * _pint - pint
+ * @stack: stack
+ * @line: line
+ */
+
+void _pint(stack_t **stack, unsigned int line)
+{
+	if (!stack || !(*stack))
+	{
+		printf("L%u: can't pint, stack empty\n", line);
+		exit(1);
+	}
+	printf("%d\n", (*stack)->n);
+}
