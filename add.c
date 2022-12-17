2045e2d2724e441b596923826b4141748c93677d
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-17 18:24:59 -0300

    first working functions

---
+++ add.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * _add - adds a node
+ * @stack: stack
+ * @line
+ */
+
+void _add(stack_t **stack, unsigned int line)
+{
+	stack_t *node = NULL;
+	int sum;
+
+	if (!stack || !(*stack) || !(*stack)->next)
+	{
+		printf("L%u: can't add, stack too short\n", line);
+		exit(1);
+	}
+	node = *stack;
+	sum = node->n + node->next->n;
+	node->next->n = sum;
+	*stack = node->next;
+	free(node);
+}
