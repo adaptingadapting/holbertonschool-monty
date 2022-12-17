3ff18cca952721daddfd9231edeaf571d0167158
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-17 18:25:16 -0300

    first working protype

---
+++ checkifvaild.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * checkifvalid - checks if valid
+ * @stack: stack
+ * @op: opcode
+ * @line: line number
+ */
+
+void checkifvalid(stack_t **stack, unsigned int line, char *op)
+{
+	int i;
+	instruction_t ops[] = {
+	  {"push", _push},
+	  {"pall", _pall},
+	  {"pint", _pint},
+	  {"pop", _pop},
+	  {"swap", _swap},
+	  {"add", _add},
+	  {"nop", _nop},
+	  {NULL, NULL}
+	};
+	for (i = 0; ops[i].opcode; i++)
+		if (!strcmp(op, ops[i].opcode))
+		{
+			ops[i].f(stack, line);
+			return;
+		}
+	if (op && op[0] != '#')
+	{
+		printf("L%u: unkown intruction %s\n", line, op);
+		exit(1);
+	}
+}
