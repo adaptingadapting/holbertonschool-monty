3770a26a0b692139cf2f07b6fcdcbb0767d057ca
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-17 19:30:35 -0300

    hotfix

--- checkifvaild.c
+++ checkifvaild.c
@@ -28,7 +28,7 @@ void checkifvalid(stack_t **stack, unsigned int line, char *op)
 		}
 	if (op && op[0] != '#')
 	{
-		fprintf(stderr, "L%u: unkown intruction %s\n", line, op);
+		fprintf(stderr, "L%u: unknown instruction %s\n", line, op);
 		exit(1);
 	}
 }
