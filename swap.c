01e91bde7c071f12201463e7f690d93d670adf42
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-17 18:26:30 -0300

    swaps the first two elements of the stack

---
+++ swap.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * _swap - swaps
+ * @stack: stack
+ * @line: number
+ */
+
+void _swap(stack_t **stack, unsigned int line)
+{
+	stack_t *node = NULL;
+
+	if (!stack || !(*stack) || !(*stack)->next)
+	{
+		printf("L%u: can't swap, stack too short\n", line);
+		exit(1);
+	}
+	node = (*stack)->next;
+	(*stack)->prev = node;
+	(*stack)->next = node->next;
+	node->prev = NULL;
+	if (node->next)
+		node->next->prev = *stack;
+	node->next = *stack;
+	*stack = node;
+}
