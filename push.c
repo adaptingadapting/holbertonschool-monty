39dadd9a16746c032db40a43e732711f0396b104
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-17 19:30:13 -0300

    fixed the case where the first char of ntopush is a -

--- push.c
+++ push.c
@@ -3,14 +3,14 @@
 /**
  * _push - pushes new node
  * @stack: stack
- * @line_number: the line number
+ * @line: the line number
  */
 
 void _push(stack_t **stack, unsigned int line)
 {
 	stack_t *node;
 	char *ntopush;
-	int i;
+	int i = 0;
 	int sum;
 
 	ntopush = strtok(NULL, delim);
@@ -19,14 +19,16 @@ void _push(stack_t **stack, unsigned int line)
 		fprintf(stderr, "L%u: usage: push integer\n", line);
 		exit(1);
 	}
-	
+
 	node = malloc(sizeof(stack_t));
 	if (!node)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(1);
 	}
-	for (i = 0; ntopush[i]; i++)
+	if (ntopush[0] == '-')
+		i = 1;
+	for (; ntopush[i]; i++)
 	{
 		sum = ntopush[i] - '0';
 		if ((sum > 9 || sum < 0))
