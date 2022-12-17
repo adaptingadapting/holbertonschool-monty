2322beac99cc0826b9e7b32a2d2a13a7bed1367c
root <root@LAPTOP-V31VOACE>
2022-12-17 16:42:38 -0300

    good version of push

--- push.c
+++ push.c
@@ -1,10 +1,32 @@
 #include "monty.h"
 
-int _push(stack_t **stack, unsigned int line_number)
+/**
+ * _push - pushes new node
+ * @stack: stack
+ * @line_number: the line number
+ */
+
+void _push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *node;
-	
-	node->next = NULL;
+	char *ntopush;
+
+	ntopush = strtok(NULL, delim);
+	if (!ntopush)
+	{
+		printf("L%u: usage: push integer\n", line_number);
+		exit(1);
+	}
+	node = malloc(sizeof(stack_t));
+	if (!node)
+	{
+		printf("Error: malloc failed\n");
+		exit(1);
+	}
+	node->n = atoi(ntopush);
 	node->prev = NULL;
-	node->n = 0;
+	node->next = *stack;
+	if (*stack)
+		(*stack)->prev = node;
+	*stack = node;
 }
