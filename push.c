1e72c18e9a243e6e8bfca8716e4dc85cdc2d5a7f
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-17 18:55:39 -0300

    now pinrts to stderr

--- push.c
+++ push.c
@@ -14,13 +14,13 @@ void _push(stack_t **stack, unsigned int line_number)
 	ntopush = strtok(NULL, delim);
 	if (!ntopush)
 	{
-		printf("L%u: usage: push integer\n", line_number);
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(1);
 	}
 	node = malloc(sizeof(stack_t));
 	if (!node)
 	{
-		printf("Error: malloc failed\n");
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(1);
 	}
 	node->n = atoi(ntopush);
