a3a558eb6d64668f7917db12719445a695396714
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-17 18:27:15 -0300

    modified smaller version, now file handling is done by another function

--- main.c
+++ main.c
@@ -1,35 +1,23 @@
 #include "monty.h"
+  
+/**
+ * main - project
+ * @argv: no
+ * @argc: count
+ * Return: yes
+ */
 
 int main(int argc, char *argv[])
 {
-  FILE *fp;
-  char *line = NULL;
-  size_t len = 0;
-  ssize_t nread;
-  char *token[80] = {0};
-  int i = 0;
-  int (*f)(int);
-	if (argc > 2)
+	stack_t *head;
+
+	if (argc!= 2)
 	{
-		printf("too mnay args\n");
+		printf("USAGE: monty file\n");
 		exit(1);
 	}
-  	fp = fopen(argv[1], "r");
-	if (!fp)
-		exit(EXIT_FAILURE);
-	while ((nread = getline(&line, &len, fp)) != -1)
-	{
-		token[0] = strtok(line, " \t\n");
-		token[1] = strtok(NULL, " \t\n");
-		if (token[1])
-			printf("%s %s", token[0], token[1]);
-		else
-			printf("%s", token[0]);
-		printf("\n");
-		f = formatFunc(token[0]);
-	}
-	free(line);
-	fclose(fp);
-	exit(EXIT_SUCCESS);
-}
+	head = NULL;
 
+	read_file(argv[1], &head);
+	exit(0);
+}
